3fd808f81f6b416d5af76c224b71685ee3dcfdf4
krisna ahroid <krisna@clappingape.com>
2018-10-02 21:15:53 +0700

    feature mega menu

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -11,4 +11,23 @@ $(document).ready(function () {
     prevEl: '.swiper-button-prev',
   },
 });
+
+// filter by index abc
+
+    $('#filter-by>span>a').click(function(){
+        var key = $(this).text();
+        if(key == 'All') {
+            $('#filtered>.persons>ul>li>a').show();
+            return;
+        }
+        $('#filtered>.persons>ul>li>a').hide();
+        $('#filtered>.persons>ul>li>a:content(^' + key +')').show()
+    })
+
+    $.extend($.expr[':'], {
+       'content': function(elem, i, attr){
+         return( RegExp(attr[3]).test($(elem).text()) );
+       }
+    });
+
 });
